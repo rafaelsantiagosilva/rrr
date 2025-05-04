6498cf1f42ccae4a3484eadcda07b217e3a67329
rafaelsantiagosilva <rafael.santiago.silva.1405@gmail.com>
2025-05-03 22:51:14 -0300

    feat: fazendo com que o swagger.json seja gerado sempre que o server iniciar

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,25 @@
 import { fastifyCors } from '@fastify/cors'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUi from '@fastify/swagger-ui'
-import fastify from 'fastify'
+import fastify, { FastifyInstance } from 'fastify'
+
 import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import {
   jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
+
+import { writeFile } from 'fs/promises'
+import { join, resolve } from 'path'
+
+function createSwaggerJson(app: FastifyInstance) {
+  const spec = app.swagger();
+  const ROOT_PATH = join(__dirname, '..');
+  const FILE_NAME = "swagger.json";
+
+  writeFile(resolve(ROOT_PATH, FILE_NAME), JSON.stringify(spec, null, 2), "utf8");
+}
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -33,6 +45,9 @@ app.register(fastifySwaggerUi, {
   routePrefix: '/api-docs',
 })
 
+app.ready().then(() => {
+  createSwaggerJson(app);
+});
 
 export { app }
 
